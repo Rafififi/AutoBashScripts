d4ac3c90df000776bd06fe62608808380eb18f1c
Rafififi <rtoameh@gmail.com>
2024-03-28 11:35:53 -0400

    deleted:    AutoBashScripts 	modified:   AutoCompile.sh 	modified:   AutoRun.sh 	modified:   AutoVal.sh

--- AutoCompile.sh
+++ AutoCompile.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
-file_name=$1
+read file_name
+echo ""
+# I want the file to be passed in as an argument
 
-if [[ -z "$file_name" ]]; then
-    echo "Usage: $0 <file_name>"
-    exit 1
+if [[ $file_name == *.cpp ]]
+then
+    g++ $file_name -o ${file_name%.cpp}
+elif [[ $file_name == *.c ]]
+then
+    gcc $file_name -o ${file_name%.c}
+
+elif [[ $file_name == *.py ]]
+then
+    python3 $file_name
+elif [[ $file_name == *.rs ]]
+then
+    rustc $file_name
+elif [[ $file_name == *.asm ]]
+then
+    as $file_name -o ${file_name%.asm}.o
+    gcc ${file_name%.asm}.o -o ${file_name%.asm} -nostdlib -static
+    rm ${file_name%.asm}.o
+
+elif [[ $file_name == *.js ]]
+then
+  node $file_name
+elif [[ $file_name == *.ts ]]
+then
+  tsc $file_name
+else
+    echo "File type not supported"
 fi
-
-extension="${file_name##*.}"
-
-case "$extension" in
-    "cpp")
-        g++ "$file_name" -o "${file_name%.cpp}"
-        ;;
-    "c")
-        gcc "$file_name" -o "${file_name%.c}"
-        ;;
-    "py")
-        python3 "$file_name"
-        ;;
-    "rs")
-        rustc "$file_name"
-        ;;
-    "asm")
-        as "$file_name" -o "${file_name%.asm}.o"
-        gcc "${file_name%.asm}.o" -o "${file_name%.asm}" -nostdlib -static
-        rm "${file_name%.asm}.o"
-        ;;
-    "js")
-        node "$file_name"
-        ;;
-    "ts")
-        tsc "$file_name"
-        ;;
-    *)
-        echo "File type not supported"
-        ;;
-esac
--- AutoVal.sh
+++ AutoVal.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-file_name=$1
+read file_name
+# I want the file to be passed in as an argument
 
-if [[ -z "$file_name" ]]; then
-    echo "Usage: $0 <file_name>"
-    exit 1
+echo ""
+
+if [[ $file_name == *.cpp ]]
+then
+  valgrind --leak-check=full --show-reachable=yes --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./${file_name%.cpp}
+elif [[ $file_name == *.c ]]
+then
+  valgrind --leak-check=full --show-reachable=yes --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./${file_name%.c}
+
+elif [[ $file_name == *.rs ]]
+then
+  valgrind --leak-check=full --show-reachable=yes --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./${file_name%.rs}
+elif [[ $file_name == *.asm ]]
+then
+  valgrind --leak-check=full --show-reachable=yes --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./${file_name%.asm}
 fi
-
-extension="${file_name##*.}"
-
-case "$extension" in
-    "cpp" | "c" | "rs" | "asm")
-        valgrind --leak-check=full --show-reachable=yes --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt "./$file_name"
-        ;;
-    *)
-        echo "File type not supported"
-        ;;
-esac
